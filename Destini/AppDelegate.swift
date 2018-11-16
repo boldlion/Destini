ee3c2067ea97c4a71cc53be0be24ad68a76942b7
Dani <dgerdzhikova@gmail.com>
2018-11-16 09:51:36 +0200

    Finished project

---
+++ Destini/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Destini
+//
+//  Created by Bold Lion on 12/11/2018.
+//  Copyright (c) 2018 Destini. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
